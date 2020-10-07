489f277996db0c8dfd8933ce27b07032f83e56c0
DHeld000 <dave.held@comcast.com>
2020-10-06 17:28:12 -0700

    chore(dummy-data): stub data

---
+++ client/src/config/dumbo-data.js
@@ -0,0 +1,57 @@
+const DumboData = [
+  {
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1325851",
+    added: 1600952507000,
+    guid: "urn:theplatform:console:panel:media-metadata",
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1325851",
+    locked: false,
+    ownerId: "http://access.auth.theplatform.com/data/Account/2035709114",
+    order: 1,
+    title: "Metadata",
+    updated: 1602025796000
+  },
+  {
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1325852",
+    added: 1600952507000,
+    guid: "urn:theplatform:console:panel:media-restrictions",
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1325852",
+    locked: false,
+    ownerId: "http://access.auth.theplatform.com/data/Account/2035709114",
+    order: 2,
+    title: "Policies",
+    updated: 1600952507000
+  },
+  {
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1325853",
+    added: 1600952507000,
+    guid: "urn:theplatform:console:panel:media-detail-status",
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1325853",
+    locked: false,
+    ownerId: "http://access.auth.theplatform.com/data/Account/2035709114",
+    order: 3,
+    title: "Publish Status",
+    updated: 1600952507000
+  },
+  {
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1327023",
+    added: 1601379041000,
+    guid: "h_WOzcc7e9qu9yZa8Bv_F6yQf5HplLNn",
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1327023",
+    locked: false,
+    ownerId: "http://access.auth.theplatform.com/data/Account/2035709114",
+    order: 4,
+    title: "Custom Fields",
+    updated: 1601921956000
+  },
+  {
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1325854",
+    added: 1600952507000,
+    guid: "urn:theplatform:console:panel:media-detail-history",
+    id: "http://data.mpx.theplatform.com/cds/data/Panel/1325854",
+    locked: false,
+    ownerId: "http://access.auth.theplatform.com/data/Account/2035709114",
+    order: 5,
+    title: "History",
+    updated: 1601958466000
+  }
+];
